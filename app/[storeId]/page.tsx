3c74314d2d580b1781bc6c1d6420f56b98da9c59
Hadi Razal <hadhirasal22@gmail.com>
2024-11-01 11:11:48 +0530

    product diplaying number reduced in a row in mobile screen

--- app/[storeId]/page.tsx
+++ app/[storeId]/page.tsx
@@ -102,7 +102,7 @@ const Products = () => {
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
         </div>
       ) : (
-        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-1">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-1">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
