b678ce733076be731bd378539d3a7fc067c6fce6
Hadi Razal <hadhirasal22@gmail.com>
2024-11-09 09:34:02 +0530

    fav icon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
   },
   robots: "index, follow",
   icons: {
-    icon: "/favicon.ico",
+    icon: "/icon.ico",
   },
 };
 
@@ -58,7 +58,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         {/* Add favicon or app icon */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/public/icon.ico" sizes="any" />
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
