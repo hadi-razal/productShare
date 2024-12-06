fa0313ff685a0bb49e80534aa6d0d6cceb8b805f
Hadi_Razal <hadhirasal22@gmail.com>
2024-12-06 17:36:38 +0530

    changed the primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        'primaryColor': 'linear-gradient(to right, #3B82F6, #4F46E5)',
+        'primaryColor': '#6e41e8',
       },
     },
   },
