580b105a3b5516e25b7582e43e9a0117de565154
Hadi Razal <hadhirasal22@gmail.com>
2025-09-25 21:39:49 +0400

    updates in the types

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import localFont from "next/font/local";
-import "../globals.css";
+import "./globals.css";
 import Header from "@/components/Header";
 import ProgressBar from "@/components/ProgressBar";
 import { Toaster } from "react-hot-toast";
